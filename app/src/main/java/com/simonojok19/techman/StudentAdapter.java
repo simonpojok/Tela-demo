31e8691afdd6017db43cd28a776ffe1f0c1e78ce
Simon Peter Ojok <simonojok19@gmail.com>
2020-02-23 10:19:52 +0300

    implemented all method of recycler view

--- app/src/main/java/com/simonojok19/techman/StudentAdapter.java
+++ app/src/main/java/com/simonojok19/techman/StudentAdapter.java
@@ -1,5 +1,43 @@
 package com.simonojok19.techman;
 
-public class StudentAdapter {
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class StudentAdapter extends RecyclerView.Adapter<StudentAdapter.StudentViewHolder>{
+    private final Context context;
+
+    @NonNull
+    @Override
+    public StudentViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull StudentViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    class StudentViewHolder extends RecyclerView.ViewHolder {
+        private final TextView studentName;
+        private final TextView studentSchool;
+        private final TextView studentDistrict;
+
+        public StudentViewHolder(@NonNull View itemView) {
+            super(itemView);
+            studentName = itemView.findViewById(R.id.text_name);
+            studentSchool = itemView.findViewById(R.id.text_school);
+            studentDistrict = itemView.findViewById(R.id.text_district);
+        }
+    }
 
 }
