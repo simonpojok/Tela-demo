76e8d3e6c6509568fd28a7744dd1f86da83764fb
Simon Peter Ojok <simonojok19@gmail.com>
2020-02-23 13:00:58 +0300

    added some view model codes

--- app/src/main/java/com/simonojok19/techman/StudentViewModel.java
+++ app/src/main/java/com/simonojok19/techman/StudentViewModel.java
@@ -4,9 +4,26 @@ import android.app.Application;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
 
 public class StudentViewModel extends AndroidViewModel {
+    private StudentRepository repository;
+    private LiveData<List<Student>> students;
+
+
     public StudentViewModel(@NonNull Application application) {
         super(application);
+        repository = new StudentRepository(application);
+        students = repository.getStudents();
+    }
+
+    LiveData<List<Student>> getStudents() {
+        return students;
+    }
+
+    public void insert(Student student) {
+        repository.insert(student);
     }
 }
