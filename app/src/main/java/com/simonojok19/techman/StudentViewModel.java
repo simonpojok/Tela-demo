19b9013d45c42408b8d5c218f1e79125ccec0c4d
Simon Peter Ojok <simonojok19@gmail.com>
2020-02-23 10:13:20 +0300

    finished repository

--- app/src/main/java/com/simonojok19/techman/StudentViewModel.java
+++ app/src/main/java/com/simonojok19/techman/StudentViewModel.java
@@ -1,12 +1,30 @@
 package com.simonojok19.techman;
 
 import android.app.Application;
+import android.app.ListActivity;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
 
 public class StudentViewModel extends AndroidViewModel {
+    private StudentRepository repository;
+    private LiveData<List<Student>> allStudents;
+
+
     public StudentViewModel(@NonNull Application application) {
         super(application);
+        repository = new StudentRepository(application);
+        allStudents = repository.getStudents();
+    }
+
+    LiveData<List<Student>> getAllStudents() {
+        return allStudents;
+    }
+
+    public void insert(Student student) {
+        repository.insert(student);
     }
 }
