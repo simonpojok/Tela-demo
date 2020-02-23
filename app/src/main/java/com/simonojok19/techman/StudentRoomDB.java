b1537606858f8413fdb4f17c0f0d9e420ebb904b
Simon Peter Ojok <simonojok19@gmail.com>
2020-02-23 12:48:00 +0300

    added db class

--- app/src/main/java/com/simonojok19/techman/StudentRoomDB.java
+++ app/src/main/java/com/simonojok19/techman/StudentRoomDB.java
@@ -1,8 +1,34 @@
 package com.simonojok19.techman;
 
+import android.content.Context;
+
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Database(entities = { Student.class }, version = 1, exportSchema = false)
 public abstract class StudentRoomDB extends RoomDatabase {
+    public abstract StudentDao getStudentDao();
+
+    private static volatile StudentRoomDB INSTANCE;
+    private static final int NUMBER_OF_THREAD = 4;
+    static final ExecutorService databaseWriteExecuter =
+            Executors.newFixedThreadPool(NUMBER_OF_THREAD);
+
+
+    static StudentRoomDB getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (StudentRoomDB.class) {
+                INSTANCE = Room.databaseBuilder(
+                        context.getApplicationContext(),
+                        StudentRoomDB.class,
+                        "student_databse"
+                ).build();
+            }
+        }
+        return INSTANCE;
+    }
 }
