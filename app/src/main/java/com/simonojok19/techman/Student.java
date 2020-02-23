eb7aa83488b56bd100bd7b78337aa8a085dfe587
Simon Peter Ojok <simonojok19@gmail.com>
2020-02-23 12:31:21 +0300

    added all the students attributes

--- app/src/main/java/com/simonojok19/techman/Student.java
+++ app/src/main/java/com/simonojok19/techman/Student.java
@@ -1,11 +1,25 @@
 package com.simonojok19.techman;
 
 import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
 
+@Entity(tableName = "student_table")
 public class Student {
+
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "name")
     private String studentName;
+
+    @ColumnInfo(name = "class")
     private String studentClass;
+
+    @ColumnInfo(name = "school")
     private String studentSchool;
+
+    @ColumnInfo(name = "district")
     private String studentDistrict;
 
     public Student(@NonNull String studentName, String studentClass, String studentSchool, String studentDistrict) {
@@ -13,5 +27,38 @@ public class Student {
         this.studentDistrict = studentDistrict;
         this.studentClass = studentClass;
         this.studentSchool = studentSchool;
+    }
+
+    @NonNull
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(@NonNull String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getStudentClass() {
+        return studentClass;
+    }
+
+    public void setStudentClass(String studentClass) {
+        this.studentClass = studentClass;
+    }
+
+    public String getStudentSchool() {
+        return studentSchool;
+    }
+
+    public void setStudentSchool(String studentSchool) {
+        this.studentSchool = studentSchool;
+    }
+
+    public String getStudentDistrict() {
+        return studentDistrict;
+    }
+
+    public void setStudentDistrict(String studentDistrict) {
+        this.studentDistrict = studentDistrict;
     }
 }
