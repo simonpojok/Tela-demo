0c11c945ecb90711b917e5b95d4d4a114621de7e
Simon Peter Ojok <simonojok19@gmail.com>
2020-02-23 13:34:53 +0300

    reimplemented the recycler view

--- app/src/main/java/com/simonojok19/techman/StudentAdaper.java
+++ app/src/main/java/com/simonojok19/techman/StudentAdaper.java
@@ -1,4 +1,32 @@
 package com.simonojok19.techman;
 
-public class StudentAdaper {
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class StudentAdaper extends RecyclerView.Adapter<StudentAdaper.StudentViewHold> {
+
+    public class StudentViewHold extends RecyclerView.ViewHolder{
+        public StudentViewHold(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
+    @NonNull
+    @Override
+    public StudentViewHold onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull StudentViewHold holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 }
