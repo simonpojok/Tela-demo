b20489ed0999d0986d1aeede228bab996679bf21
Simon Peter Ojok <simonojok19@gmail.com>
2020-02-23 09:38:11 +0300

    added dao

---
+++ app/src/main/java/com/simonojok19/techman/StudentDao.java
@@ -0,0 +1,26 @@
+package com.simonojok19.techman;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import org.stringtemplate.v4.ST;
+
+import java.util.List;
+
+@Dao
+public interface StudentDao {
+    @Query("SELECT * FROM student_table")
+    List<Student> getAll();
+
+    @Insert
+    void insert(Student student);
+
+    @Delete
+    void delete(Student student);
+
+    @Update
+    void update(Student student);
+}
