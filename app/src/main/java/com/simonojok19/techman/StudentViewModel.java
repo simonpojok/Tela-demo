eb2db35af417a0cf481c7eb9a5b6ffee48cb8896
Simon Peter Ojok <simonojok19@gmail.com>
2020-02-23 10:10:32 +0300

    added view model

---
+++ app/src/main/java/com/simonojok19/techman/StudentViewModel.java
@@ -0,0 +1,12 @@
+package com.simonojok19.techman;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+public class StudentViewModel extends AndroidViewModel {
+    public StudentViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
