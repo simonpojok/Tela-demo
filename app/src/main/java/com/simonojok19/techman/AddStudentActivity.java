45c7fab1ca0521bbfebcc599a7b145bb778c62b9
Simon Peter Ojok <simonojok19@gmail.com>
2020-02-23 11:07:02 +0300

    added some codes

--- app/src/main/java/com/simonojok19/techman/AddStudentActivity.java
+++ app/src/main/java/com/simonojok19/techman/AddStudentActivity.java
@@ -2,13 +2,35 @@ package com.simonojok19.techman;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AddStudentActivity extends AppCompatActivity {
+    private EditText studentName;
+    private EditText studentClass;
+    private EditText studentSchool;
+    private EditText studentDistrict;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_student);
+        studentName = findViewById(R.id.student_name);
+        studentClass = findViewById(R.id.student_class);
+        studentSchool = findViewById(R.id.student_school);
+        studentDistrict = findViewById(R.id.student_district);
+
+        final Button button = findViewById(R.id.button_save);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =  new Intent();
+
+
+            }
+        });
     }
 }
