df18c0d6b5a1cfffd72676b5925b135cf578e496
Simon Peter Ojok <simonojok19@gmail.com>
2020-02-23 13:45:23 +0300

    refactered recycler adapter

--- app/src/main/java/com/simonojok19/techman/StudentAdaper.java
+++ app/src/main/java/com/simonojok19/techman/StudentAdaper.java
@@ -1,32 +1,69 @@
 package com.simonojok19.techman;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
 public class StudentAdaper extends RecyclerView.Adapter<StudentAdaper.StudentViewHold> {
 
     public class StudentViewHold extends RecyclerView.ViewHolder{
+        private final TextView studentName;
+        private final TextView studentSchool;
+        private final TextView studentDistrict;
+
         public StudentViewHold(@NonNull View itemView) {
             super(itemView);
+            studentName = itemView.findViewById(R.id.text_name);
+            studentSchool = itemView.findViewById(R.id.text_school);
+            studentDistrict = itemView.findViewById(R.id.text_district);
         }
+
+        public void bindStudent(Student student) {
+            studentName.setText(student.getStudentName());
+            studentSchool.setText(student.getStudentSchool());
+            studentDistrict.setText(student.getStudentDistrict());
+        }
+    }
+
+    private final LayoutInflater layoutInflater;
+    private List<Student> students;
+
+
+    StudentAdaper(Context context) {
+        layoutInflater = LayoutInflater.from(context);
     }
 
     @NonNull
     @Override
     public StudentViewHold onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = layoutInflater.inflate(R.layout.recyclerview_student, parent, false);
+        return new StudentViewHold(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull StudentViewHold holder, int position) {
-
+        if (students != null) {
+            Student current = students.get(position);
+            holder.bindStudent(current);
+        }
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        if (students != null) {
+            return students.size();
+        } else return 0;
+    }
+
+    void setStudents(List<Student> students) {
+        this.students = students;
+        notifyDataSetChanged();
     }
 }
