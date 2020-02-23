57433f1c80de2a9b52c880bb7670e1b01d4f2a8a
Simon Peter Ojok <simonojok19@gmail.com>
2020-02-23 12:29:03 +0300

    added student back

--- app/src/main/java/com/simonojok19/techman/Student.java
+++ app/src/main/java/com/simonojok19/techman/Student.java
@@ -1,4 +1,17 @@
 package com.simonojok19.techman;
 
+import androidx.annotation.NonNull;
+
 public class Student {
+    private String studentName;
+    private String studentClass;
+    private String studentSchool;
+    private String studentDistrict;
+
+    public Student(@NonNull String studentName, String studentClass, String studentSchool, String studentDistrict) {
+        this.studentName = studentName;
+        this.studentDistrict = studentDistrict;
+        this.studentClass = studentClass;
+        this.studentSchool = studentSchool;
+    }
 }
