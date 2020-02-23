d5b41b4238a81ff4d8b89744ab959625241fe305
Simon Peter Ojok <simonojok19@gmail.com>
2020-02-23 08:49:11 +0300

    added update student activity

---
+++ app/src/main/java/com/simonojok19/techman/UpdateStudentActivity.java
@@ -0,0 +1,14 @@
+package com.simonojok19.techman;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UpdateStudentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_student);
+    }
+}
