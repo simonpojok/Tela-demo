69d50e05072569dcceb79c1cf37cb5adabf6180f
Simon Peter Ojok <simonojok19@gmail.com>
2020-02-23 11:12:30 +0300

    finised add student

--- app/src/main/java/com/simonojok19/techman/AddStudentActivity.java
+++ app/src/main/java/com/simonojok19/techman/AddStudentActivity.java
@@ -14,6 +14,12 @@ public class AddStudentActivity extends AppCompatActivity {
     private EditText studentSchool;
     private EditText studentDistrict;
 
+    public static final String STUDENT_NAME = "com.simonojok19.techman.STUDENT_NAME";
+    public static final String STUDENT_CLASS = "com.simonojok19.techman.STUDENT_CLASS";
+    public static final String STUDENT_SCHOOL = "com.simonojok19.techman.STUDENT_SCHOOL";
+    public static final String STUDENT_DISTRICT = "com.simonojok19.techman.STUDENT_DISTRICT";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,8 +34,12 @@ public class AddStudentActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent =  new Intent();
-
-
+                intent.putExtra(STUDENT_NAME, studentName.getText().toString());
+                intent.putExtra(STUDENT_CLASS, studentClass.getText().toString());
+                intent.putExtra(STUDENT_SCHOOL, studentSchool.getText().toString());
+                intent.putExtra(STUDENT_DISTRICT, studentDistrict.getText().toString());
+                setResult(RESULT_OK, intent);
+                finish();
             }
         });
     }
