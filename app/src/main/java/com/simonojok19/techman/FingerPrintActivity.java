d1306c7a4dddf0f2601b033782f4ac45bd6efd7a
Simon Peter Ojok <simonojok19@gmail.com>
2020-02-28 08:35:02 +0300

    added response

--- app/src/main/java/com/simonojok19/techman/FingerPrintActivity.java
+++ app/src/main/java/com/simonojok19/techman/FingerPrintActivity.java
@@ -9,6 +9,7 @@ import android.graphics.Bitmap;
 import android.hardware.usb.UsbManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.suprema.BioMiniFactory;
 import com.suprema.CaptureResponder;
@@ -17,6 +18,59 @@ import com.suprema.IBioMiniDevice;
 import java.util.Locale;
 
 public class FingerPrintActivity extends AppCompatActivity {
+    //Flag.
+    public static final boolean mbUsbExternalUSBManager = false;
+    private static final String ACTION_USB_PERMISSION = "com.android.example.USB_PERMISSION";
+    private UsbManager mUsbManager = null;
+    private PendingIntent mPermissionIntent= null;
+    //
+
+    private static BioMiniFactory mBioMiniFactory = null;
+    public static final int REQUEST_WRITE_PERMISSION = 786;
+    public IBioMiniDevice mCurrentDevice = null;
+    private FingerPrintActivity mainContext;
+
+    private IBioMiniDevice.CaptureOption mCaptureOptionDefault = new IBioMiniDevice.CaptureOption();
+    private CaptureResponder mCaptureResponseDefault = new CaptureResponder() {
+        @Override
+        public boolean onCaptureEx(final Object context, final Bitmap capturedImage,
+                                   final IBioMiniDevice.TemplateData capturedTemplate,
+                                   final IBioMiniDevice.FingerState fingerState) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(FingerPrintActivity.this, "Success in Capture", Toast.LENGTH_SHORT).show();
+                }
+            });
+            return true;
+        }
+
+        @Override
+        public void onCaptureError(Object contest, int errorCode, String error) {
+            Toast.makeText(FingerPrintActivity.this, "Error in Capture", Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    private CaptureResponder mCaptureResponsePrev = new CaptureResponder() {
+        @Override
+        public boolean onCaptureEx(final Object context, final Bitmap capturedImage,
+                                   final IBioMiniDevice.TemplateData capturedTemplate,
+                                   final IBioMiniDevice.FingerState fingerState) {
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(FingerPrintActivity.this, "Success in Capture 2", Toast.LENGTH_SHORT).show();
+                }
+            });
+            return true;
+        }
+
+        @Override
+        public void onCaptureError(Object context, int errorCode, String error) {
+            Toast.makeText(FingerPrintActivity.this, "Error in Capture 2", Toast.LENGTH_SHORT).show();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
