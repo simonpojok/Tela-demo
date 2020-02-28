a18d6090b4ee5120f615a1231122f829ea87af2f
Simon Peter Ojok <simonojok19@gmail.com>
2020-02-28 08:27:45 +0300

    ready

--- app/src/main/java/com/simonojok19/techman/FingerPrintActivity.java
+++ app/src/main/java/com/simonojok19/techman/FingerPrintActivity.java
@@ -2,7 +2,19 @@ package com.simonojok19.techman;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.hardware.usb.UsbManager;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.suprema.BioMiniFactory;
+import com.suprema.CaptureResponder;
+import com.suprema.IBioMiniDevice;
+
+import java.util.Locale;
 
 public class FingerPrintActivity extends AppCompatActivity {
 
