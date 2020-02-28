7c9c6a12efb8c403e31806e176d32e2a5293312e
Simon Peter Ojok <simonojok19@gmail.com>
2020-02-28 10:26:58 +0300

    copied strings

--- app/src/main/java/com/simonojok19/techman/EnrollActivity.java
+++ app/src/main/java/com/simonojok19/techman/EnrollActivity.java
@@ -2,9 +2,70 @@ package com.simonojok19.techman;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.PendingIntent;
+import android.graphics.Bitmap;
+import android.hardware.usb.UsbManager;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ScrollView;
+
+import com.suprema.BioMiniFactory;
+import com.suprema.CaptureResponder;
+import com.suprema.IBioMiniDevice;
 
 public class EnrollActivity extends AppCompatActivity {
+    //Flag.
+    public static final boolean mbUsbExternalUSBManager = false;
+    private static final String ACTION_USB_PERMISSION = "com.android.example.USB_PERMISSION";
+    private UsbManager mUsbManager = null;
+    private PendingIntent mPermissionIntent= null;
+    //
+
+    private static BioMiniFactory mBioMiniFactory = null;
+    public static final int REQUEST_WRITE_PERMISSION = 786;
+    public IBioMiniDevice mCurrentDevice = null;
+    private EnrollActivity mainContext;
+
+    public final static String TAG = "BioMini Sample";
+    private EditText mLogView;
+    private ScrollView mScrollLog = null;
+
+    private IBioMiniDevice.CaptureOption mCaptureOptionDefault = new IBioMiniDevice.CaptureOption();
+    private CaptureResponder mCaptureResponseDefault = new CaptureResponder() {
+        @Override
+        public boolean onCaptureEx(final Object context, final Bitmap capturedImage,
+                                   final IBioMiniDevice.TemplateData capturedTemplate,
+                                   final IBioMiniDevice.FingerState fingerState) {
+            log("onCapture : Capture successful!");
+            printState(getResources().getText(R.string.capture_single_ok));
+
+            log(((IBioMiniDevice) context).popPerformanceLog());
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if(capturedImage != null) {
+                        ImageView iv = (ImageView) findViewById(R.id.imagePreview);
+                        if(iv != null) {
+                            iv.setImageBitmap(capturedImage);
+                        }
+                    }
+                }
+            });
+            return true;
+        }
+
+        @Override
+        public void onCaptureError(Object contest, int errorCode, String error) {
+            log("onCaptureError : " + error + " ErrorCode :" + errorCode);
+            if( errorCode != IBioMiniDevice.ErrorCode.OK.value())
+                printState(getResources().getText(R.string.capture_single_fail) + "("+error+")");
+        }
+    };
+
+    private void log(String s) {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
