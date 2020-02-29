bf83acaf297c839f04874c622aecc647e2b8c923
Simon Peter Ojok <simonojok19@gmail.com>
2020-02-29 23:39:17 +0300

    added attendance

---
+++ app/src/main/java/co/planetsystems/tela/Attendance.java
@@ -0,0 +1,35 @@
+package co.planetsystems.tela;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "attendance")
+public class Attendance {
+    @ColumnInfo(name = "id")
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @ColumnInfo(name = "date")
+    private String date;
+
+    @ColumnInfo(name = "in")
+    private String clockIn;
+
+    @ColumnInfo(name = "in_bytes")
+    private String inBytes;
+
+    @ColumnInfo(name = "in_bitmap")
+    private String inBitmap;
+
+    @ColumnInfo(name = "out")
+    private String clockOut;
+
+    @ColumnInfo(name = "out_bytes")
+    private String outBytes;
+
+    @ColumnInfo(name = "out_bitmap")
+    private String outBitmap;
+
+
+}
