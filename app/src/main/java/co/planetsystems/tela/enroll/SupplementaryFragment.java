84f8fac06ea17717c3c78b63c14e60c528f77b1f
Simon Peter Ojok <simonojok19@gmail.com>
2020-03-01 12:34:05 +0300

    added a new fragment

---
+++ app/src/main/java/co/planetsystems/tela/enroll/SupplementaryFragment.java
@@ -0,0 +1,42 @@
+package co.planetsystems.tela.enroll;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import co.planetsystems.tela.R;
+
+public class SupplementaryFragment extends Fragment {
+    public SupplementaryFragment() {
+        // Required empty public constructor
+    }
+
+    public static BasicFragment newInstance() {
+        return new BasicFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_supplementary, container, false);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
