43b53a3cccc20e0a34999dfd39665504bab603b0
Simon Peter Ojok <simonojok19@gmail.com>
2020-03-01 08:06:06 +0300

    added another class

--- app/src/main/java/co/planetsystems/tela/TeacherAttendance.java
+++ app/src/main/java/co/planetsystems/tela/TeacherAttendance.java
@@ -1,4 +1,35 @@
 package co.planetsystems.tela;
 
+import androidx.room.Embedded;
+import androidx.room.Relation;
+
 public class TeacherAttendance {
+    @Embedded
+    private Teacher teacher;
+    @Relation(
+            parentColumn = "nationID",
+            entityColumn = "id"
+    )
+    private Attendance attendance;
+
+    public TeacherAttendance(Teacher teacher, Attendance attendance) {
+        this.teacher = teacher;
+        this.attendance = attendance;
+    }
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+
+    public Attendance getAttendance() {
+        return attendance;
+    }
+
+    public void setAttendance(Attendance attendance) {
+        this.attendance = attendance;
+    }
 }
