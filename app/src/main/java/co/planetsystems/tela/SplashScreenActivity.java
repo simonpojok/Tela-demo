7b7a4142f4b3b62ba63bd898455cca1e53c4744b
Simon Peter Ojok <simonojok19@gmail.com>
2020-03-01 19:48:10 +0300

    added color changes

--- app/src/main/java/co/planetsystems/tela/SplashScreenActivity.java
+++ app/src/main/java/co/planetsystems/tela/SplashScreenActivity.java
@@ -2,7 +2,9 @@ package co.planetsystems.tela;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 5*1000);
     }
 }
