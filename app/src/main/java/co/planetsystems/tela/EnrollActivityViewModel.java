e239b43dd45598e75fe436533d25b9acecd2ac48
Simon Peter Ojok <simonojok19@gmail.com>
2020-03-01 13:01:00 +0300

    added the view model

--- app/src/main/java/co/planetsystems/tela/EnrollActivityViewModel.java
+++ app/src/main/java/co/planetsystems/tela/EnrollActivityViewModel.java
@@ -6,7 +6,17 @@ import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 
 public class EnrollActivityViewModel extends AndroidViewModel {
+    private int fragmentPosition = 1;
+
     public EnrollActivityViewModel(@NonNull Application application) {
         super(application);
+    }
+
+    public int getFragmentPosition() {
+        return fragmentPosition;
+    }
+
+    public void setFragmentPostion(int fragmentPosition) {
+        this.fragmentPosition = fragmentPostion;
     }
 }
