4c5c8bea95d760003ea86c4e79aa183b8f5f492c
Simon Peter Ojok <simonojok19@gmail.com>
2020-03-01 19:05:09 +0300

    added verified user page

---
+++ app/src/main/java/co/planetsystems/tela/VerifyActivity.java
@@ -0,0 +1,14 @@
+package co.planetsystems.tela;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VerifyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verify);
+    }
+}
