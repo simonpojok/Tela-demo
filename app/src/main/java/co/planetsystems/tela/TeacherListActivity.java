ada31b5d2414af508660596c58e2dbff41af340e
Simon Peter Ojok <simonojok19@gmail.com>
2020-03-02 00:08:16 +0300

    implemented list activity now

--- app/src/main/java/co/planetsystems/tela/TeacherListActivity.java
+++ app/src/main/java/co/planetsystems/tela/TeacherListActivity.java
@@ -3,12 +3,18 @@ package co.planetsystems.tela;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class TeacherListActivity extends AppCompatActivity {
+public class TeacherListActivity extends AppCompatActivity  implements TeacherAdapter.OnTeacherClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher_list);
+    }
+
+    @Override
+    public void onTeacherClick(Teacher teacher) {
+        Toast.makeText(this, teacher.getFirstName(), Toast.LENGTH_SHORT).show();
     }
 }
