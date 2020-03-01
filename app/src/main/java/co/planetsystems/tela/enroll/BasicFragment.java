82b63d040270247d6156bf0cf0b60f5d10aa9435
Simon Peter Ojok <simonojok19@gmail.com>
2020-03-01 13:47:26 +0300

    passing data

--- app/src/main/java/co/planetsystems/tela/enroll/BasicFragment.java
+++ app/src/main/java/co/planetsystems/tela/enroll/BasicFragment.java
@@ -14,19 +14,33 @@ import co.planetsystems.tela.R;
 
 
 public class BasicFragment extends Fragment {
+    private static final String FIRST_NAME = "co.planetsystems.tela.BasicFragment.FIRST_NAME";
+    private static final String LAST_NAME = "co.planetsystems.tela.BasicFragment.LAST_NAME";
+    private static final String PHONE_NUMBER = "co.planetsystems.tela.BasicFragment.PHONE_NUMBER";
+
 
 
     public BasicFragment() {
         // Required empty public constructor
     }
 
-    public static BasicFragment newInstance() {
-        return new BasicFragment();
+    public static BasicFragment newInstance(String firstName, String lastName, String phoneNumber) {
+        BasicFragment fragment = new BasicFragment();
+        Bundle args = new Bundle();
+        args.putString(FIRST_NAME, firstName);
+        args.putString(LAST_NAME, lastName);
+        args.putString(PHONE_NUMBER, phoneNumber);
+        fragment.setArguments(args);
+        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
     }
 
     @Override
