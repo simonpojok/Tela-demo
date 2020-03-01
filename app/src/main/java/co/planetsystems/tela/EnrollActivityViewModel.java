586c45636f0648dc8914607bfab18d2b0dbd1f32
Simon Peter Ojok <simonojok19@gmail.com>
2020-03-01 13:35:38 +0300

    added fields

--- app/src/main/java/co/planetsystems/tela/EnrollActivityViewModel.java
+++ app/src/main/java/co/planetsystems/tela/EnrollActivityViewModel.java
@@ -8,8 +8,8 @@ import androidx.lifecycle.AndroidViewModel;
 public class EnrollActivityViewModel extends AndroidViewModel {
     private String firstName;
     private String lastName;
-    private String phone_number;
-    private String email_address;
+    private String phoneNumber;
+    private String emailAddress;
     private String gender;
     private String nationalId;
     private String school;
@@ -20,4 +20,76 @@ public class EnrollActivityViewModel extends AndroidViewModel {
         super(application);
     }
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getNationalId() {
+        return nationalId;
+    }
+
+    public void setNationalId(String nationalId) {
+        this.nationalId = nationalId;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
 }
