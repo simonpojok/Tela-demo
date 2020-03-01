2e4bc1e1b6169ec135c2ac4bf276d08ce7d2c1cd
Simon Peter Ojok <simonojok19@gmail.com>
2020-03-01 23:48:37 +0300

    finished layout

---
+++ app/src/main/java/co/planetsystems/tela/TeacherRecyclerView.java
@@ -0,0 +1,16 @@
+package co.planetsystems.tela;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class TeacherRecyclerView {
+
+    public class TeacherHolder extends RecyclerView.ViewHolder {
+
+        public TeacherHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
