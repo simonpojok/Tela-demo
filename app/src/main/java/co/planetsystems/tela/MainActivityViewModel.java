6fc2c2f58525211b41fee16c05d90129dd160be4
Simon Peter Ojok <simonojok19@gmail.com>
2020-03-01 09:40:17 +0300

    added view model

--- app/src/main/java/co/planetsystems/tela/MainActivityViewModel.java
+++ app/src/main/java/co/planetsystems/tela/MainActivityViewModel.java
@@ -1,4 +1,20 @@
 package co.planetsystems.tela;
 
-public class MainActivityViewModel {
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class MainActivityViewModel extends AndroidViewModel {
+    private TeacherRespository respository;
+    private LiveData<List<Teacher>> teachers;
+    private LiveData<List<Attendance>> attendances;
+
+
+    public MainActivityViewModel(@NonNull Application application) {
+        super(application);
+    }
 }
