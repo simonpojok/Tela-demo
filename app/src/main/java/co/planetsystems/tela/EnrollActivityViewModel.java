e447c0336095d346ac5d2154d5c59433878cfb37
Simon Peter Ojok <simonojok19@gmail.com>
2020-03-01 12:59:41 +0300

    added view model

---
+++ app/src/main/java/co/planetsystems/tela/EnrollActivityViewModel.java
@@ -0,0 +1,12 @@
+package co.planetsystems.tela;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+public class EnrollActivityViewModel extends AndroidViewModel {
+    public EnrollActivityViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
