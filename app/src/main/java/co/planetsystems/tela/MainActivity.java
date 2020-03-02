5daadc136109181d7a4534a92442edce2fe5fb66
Simon Peter Ojok <simonojok19@gmail.com>
2020-03-02 12:30:41 +0300

    error fixed

--- app/src/main/java/co/planetsystems/tela/MainActivity.java
+++ app/src/main/java/co/planetsystems/tela/MainActivity.java
@@ -22,7 +22,6 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.accessibility.AccessibilityManager;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ScrollView;
